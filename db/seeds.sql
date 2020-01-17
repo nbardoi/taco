95a77e7af97ac89d8d7ca779b22859d25a16b50e
Nagma Bardoi <nagmabardoi@gmail.com>
2020-01-16 18:43:27 -0600

    inital set-up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO tables (burger_name) VALUES ("Chile-Cheeseburger");
+INSERT INTO tables (burger_name) VALUES ("Chicken Turkey Burger");
+INSERT INTO tables (burger_name) VALUES ("Black Bean Burger");
